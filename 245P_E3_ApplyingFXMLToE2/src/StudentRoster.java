8bcb69b535f288479fba8ac53b71e9e0ae358d22
Oscar Li <58122003+oscarli9@users.noreply.github.com>
2019-11-26 11:32:51 -0800

    Add files via upload

---
+++ 245P_E3_ApplyingFXMLToE2/src/StudentRoster.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+public class StudentRoster extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StudentRosterDoc.fxml"));
+        TabPane modePane = fxmlLoader.load();
+        ((StudentRosterController)fxmlLoader.getController()).setStage(stage);
+
+        Scene scene = new Scene(modePane, 600, 780);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
